5bb803e778a79463292d9c9feedee84ff042bfd2
Gravity <gavintantleff@gmail.com>
2020-09-22 23:31:22 -0400

    Add custom config creator class

--- src/main/java/net/almostmc/FiatMachine/FiatPlugin.java
+++ src/main/java/net/almostmc/FiatMachine/FiatPlugin.java
@@ -3,5 +3,10 @@ package net.almostmc.FiatMachine;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FiatPlugin extends JavaPlugin {
+    public static FiatPlugin singleton;
 
+    @Override
+    public void onLoad() {
+        singleton = this;
+    }
 }
