119a9bbf67a40709ab6d9f7826cf181f2e49af69
Gravity <gavintantleff@gmail.com>
2020-09-26 11:00:42 -0400

    Finish GUI system
    TODOs are for when money system is built.

--- src/main/java/net/almostmc/FiatMachine/Commands/BankCommand/BankCommand.java
+++ src/main/java/net/almostmc/FiatMachine/Commands/BankCommand/BankCommand.java
@@ -76,7 +76,6 @@ public class BankCommand implements CommandExecutor {
     }
 
     private void chooseOreMenu(Player player, BankTransactionType transactionType, String prefix) {
-
         ItemMeta metaCache;
 
         ItemStack cancel = new ItemStack(Material.BARRIER);
@@ -100,6 +99,7 @@ public class BankCommand implements CommandExecutor {
         diamond.setItemMeta(metaCache);
 
         Inventory menu = new ChestGUIBuilder("Bank", ChestSize.NINE)
+                .SetItem(cancel, 0)
                 .SetItem(iron, 3)
                 .SetItem(gold, 4)
                 .SetItem(diamond, 5)
@@ -118,6 +118,93 @@ public class BankCommand implements CommandExecutor {
     }
 
     private void oreAmountMenu(Player player, BankTransactionType transactionType, BankItemType type) {
+        Material oreMat;
+        Material oreMatBlock;
+        String oreName;
+        
+        switch (type) {
+            case IRON:
+                oreMat = Material.IRON_INGOT;
+                oreMatBlock = Material.IRON_BLOCK;
+                oreName = "Iron";
+                break;
+            case GOLD:
+                oreMat = Material.GOLD_INGOT;
+                oreMatBlock = Material.GOLD_BLOCK;
+                oreName = "Gold";
+                break;
+            case DIAMOND:
+                oreMat = Material.DIAMOND;
+                oreMatBlock = Material.DIAMOND_BLOCK;
+                oreName = "Diamond";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+        
+        ItemMeta metaCache;
 
+        ItemStack cancel = new ItemStack(Material.BARRIER);
+        metaCache = cancel.getItemMeta();
+        metaCache.setDisplayName("Cancel");
+        cancel.setItemMeta(metaCache);
+        
+        ItemStack ore1 = new ItemStack(oreMat, 1);
+        metaCache = ore1.getItemMeta();
+        if (type == BankItemType.IRON || type == BankItemType.GOLD)
+            metaCache.setDisplayName("1 " + oreName + " Ingot");
+        else
+            metaCache.setDisplayName("1 " + oreName);
+        ore1.setItemMeta(metaCache);
+        
+        ItemStack ore10 = new ItemStack(oreMat, 10);
+        metaCache = ore10.getItemMeta();
+        if (type == BankItemType.IRON || type == BankItemType.GOLD)
+            metaCache.setDisplayName("10 " + oreName + " Ingots");
+        else
+            metaCache.setDisplayName("10 " + oreName + "s");
+        ore10.setItemMeta(metaCache);
+
+        ItemStack ore64 = new ItemStack(oreMat, 64);
+        metaCache = ore64.getItemMeta();
+        if (type == BankItemType.IRON || type == BankItemType.GOLD)
+            metaCache.setDisplayName("64 " + oreName + " Ingots");
+        else
+            metaCache.setDisplayName("64 " + oreName + "s");
+        ore64.setItemMeta(metaCache);
+
+        ItemStack block1 = new ItemStack(oreMat, 1);
+        metaCache = block1.getItemMeta();
+        metaCache.setDisplayName("1 " + oreName + " Block");
+        block1.setItemMeta(metaCache);
+
+        ItemStack block10 = new ItemStack(oreMat, 10);
+        metaCache = block10.getItemMeta();
+        metaCache.setDisplayName("10 " + oreName + " Blocks");
+        block10.setItemMeta(metaCache);
+
+        Inventory menu = new ChestGUIBuilder("Bank", ChestSize.NINE)
+                .SetItem(cancel, 0)
+                .SetItem(ore1, 2)
+                .SetItem(ore10, 3)
+                .SetItem(ore64, 4)
+                .SetItem(block1, 5)
+                .SetItem(block10, 6)
+                .RegisterOnClickHandler(event -> {
+                    if (event.item.equals(cancel))
+                        event.clicker.closeInventory();
+                    else if (event.item.equals(ore1))
+                        return; // TODO
+                    else if (event.item.equals(ore10))
+                        return; // TODO
+                    else if (event.item.equals(ore64))
+                        return; // TODO
+                    else if (event.item.equals(block1))
+                        return; // TODO
+                    else if (event.item.equals(block10))
+                        return; // TODO
+                }).RegisterOnCloseHandler(ChestGUIBuilder::Destroy)
+                .ToInventory();
+        player.openInventory(menu);
     }
 }
