c98364361ed4d31d75b05e7bd5f61f3b168bd8af
Gravity <gavintantleff@gmail.com>
2020-09-27 22:05:35 -0400

    Basically complete MoneyManager

--- src/main/java/net/almostmc/FiatMachine/MoneyManager.java
+++ src/main/java/net/almostmc/FiatMachine/MoneyManager.java
@@ -1,51 +1,88 @@
 package net.almostmc.FiatMachine;
 
+import net.md_5.bungee.chat.SelectorComponentSerializer;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.logging.Level;
 
 public final class MoneyManager {
     static {
         FileConfiguration config = new ConfigurationBuilder("money.yml").Build();
 
-        // Probably not needed
-        /*if (config.contains("ironValue"))
-            //noinspection ConstantConditions
-            ironValue = new BigDecimal(config.getString("ironValue"));
-        else
-            ironValue = new BigDecimal("00000000.0000");
-        if (config.contains("goldValue"))
-            //noinspection ConstantConditions
-            goldValue = new BigDecimal(config.getString("goldValue"));
-        else
-            goldValue = new BigDecimal("00000000.0000");
-        if (config.contains("diamondValue"))
-            //noinspection ConstantConditions
-            diamondValue = new BigDecimal(config.getString("ironValue"));
-        else
-            diamondValue = new BigDecimal("00000000.0000");*/
-
         if (config.contains("iron"))
-            iron = config.getInt("iron");
+            iron = config.getLong("iron");
         else
             iron = 0;
         if (config.contains("gold"))
-            gold = config.getInt("gold");
+            gold = config.getLong("gold");
         else
             gold = 0;
         if (config.contains("diamond"))
-            diamond = config.getInt("diamond");
+            diamond = config.getLong("diamond");
         else
             diamond = 0;
 
         MoneyManager.config = config;
+
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            FiatPlugin.singleton.getLogger().log(Level.SEVERE, "There was an error retrieving the vault economy service. This is fatal. Shutting down");
+            Bukkit.getPluginManager().disablePlugin(FiatPlugin.singleton);
+        } else
+            economy = rsp.getProvider();
     }
 
-    private static int iron;
-    private static int gold;
-    private static int diamond;
+    private static long iron;
+    private static long gold;
+    private static long diamond;
 
     private static FileConfiguration config;
+    private static Economy economy;
 
-    public static int Iron() {return iron;}
-    public static int Gold() {return gold;}
-    public static int Diamond() {return diamond;}
+    public static long Iron() {return iron;}
+    public static long Gold() {return gold;}
+    public static long Diamond() {return diamond;}
+
+    public static void sellOre(OfflinePlayer player, int amount, BankItemType ore) {
+        if (!economy.hasAccount(player))
+            economy.createPlayerAccount(player);
+
+        for (int i = 0; i < amount; i++) {
+            economy.depositPlayer(player, calculateOreWorth(ore));
+            switch (ore) {
+                case IRON:
+                    iron += 1;
+                    break;
+                case GOLD:
+                    gold += 1;
+                    break;
+                case DIAMOND:
+                    diamond += 1;
+                    break;
+            }
+        }
+    }
+
+    private static long calculateOreWorth(BankItemType ore) {
+        long amount;
+        switch (ore) {
+            case IRON:
+                amount = iron;
+                break;
+            case GOLD:
+                amount = gold;
+                break;
+            case DIAMOND:
+                amount = diamond;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + ore);
+        }
+
+        return Math.round(((double) 50/(double) 16129)*(double) amount*2-((double)12800/(double)16129)*amount+((double) 1625650/(double) 16129));
+    }
 }
